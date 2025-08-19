286183d1765b759b38fe6815b27893300937284b
Matias Torres <mtri21019@gmail.com>
2025-08-18 21:06:32 -0300

    Add population layer and volcanoes layer.

--- index.py
+++ index.py
@@ -6,18 +6,33 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def colorProducer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
 map = folium.Map( 
-    location=[38.2,-99.1], 
-    zoom_start=12, 
+    location=[30.2,-99.1], 
+    zoom_start=6, 
     tiles="cartodb positron"
 ) 
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt,ln, el in zip(lat,lon, elev):
     popup= folium.Popup(str(el) + " m", parse_html=True)
-    fg.add_child(folium.Marker(location=[lt,ln], popup=popup, icon=folium.Icon(color='green')))
+    fgv.add_child(folium.Circle(location=[lt,ln], popup=popup, radius=80000, fill=True, fill_color=colorProducer(el), color="grey", fill_opacity=0.7))
+    
+fgp = folium.FeatureGroup(name="Population")
 
-map.add_child(fg)
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),style_function=lambda x:{'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
